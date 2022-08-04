1b758ecdc67d197c161e9710cb6d34de76e46dff
satoshi-nakajima-uhuru <nakajima.satoshi@uhuru.jp>
2022-08-04 11:28:13 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+interface CatCategory {
+  id: number;
+  name: string;
+}
+ 
+interface SearchCatImage {
+  breeds: string[];
+  categories: CatCategory[];
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+ 
+type SearchCatImageResponse = SearchCatImage[];
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = await res.json() as SearchCatImageResponse;
+  return result[0];
+}
+
+fetchCatImage().then((image) => {
+  console.log(`猫の画像: ${ image.url }`);
+})
+
+const IndexPage = () => {
+  const [catImageUrl, setCatImageUrl] = useState(
+    "https://cdn2.thecatapi.com/images/bpc.jpg"
+  );
+
+  const handleClick = async () => {
+    const image = await fetchCatImage();
+    setCatImageUrl(image.url);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>きょうのにゃんこ🐱</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} />
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
